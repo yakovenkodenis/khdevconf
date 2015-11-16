1d3c9f887bdf51a090386baf3bcceb678c127aa9
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-11-16 07:27:51 +0200

    Add devise and simple_form

--- Gemfile
+++ Gemfile
@@ -11,6 +11,10 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'devise'
+gem 'simple_form'
+gem 'administrate', '~> 0.1.1'
+
 group :development, :test do
   gem 'better_errors'
   gem 'binding_of_caller'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # :confirmable, :lockable, :timeoutable, :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :lockable
+
   has_many :feedbacks
   has_one :sponsorship_plan
   has_one :user_type, foreign_key: 'user_type_id'
