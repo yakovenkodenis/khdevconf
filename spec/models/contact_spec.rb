20d8ec9b3da1889503e206782c3cad8087e40699
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-11-16 09:47:29 +0200

    Add tests for contact

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -13,5 +13,12 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :contact }
+  end
+
+  describe 'associations' do
+    it { should have_one :contact_type }
+    it { should belong_to :organisers_contact }
+  end
 end
