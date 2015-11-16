93a935aff6ced50190aa66e293f03b60bffa1352
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-11-16 09:38:05 +0200

    Add feedback tests

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -12,5 +12,12 @@
 require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :feedback }
+    it { should_validate_presence_of :user }
+  end
+
+  describe 'associations' do
+    it { should belong_to :user }
+  end
 end
